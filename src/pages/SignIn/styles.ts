126ce07cf44010ab0fb86d69a42398e45e1dd739
Renato Bessa <bessache@outlook.com>
2020-05-02 14:12:10 -0300

    commit aula isolando componentes

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -19,63 +19,33 @@ export const Content = styled.div`
     width: 340px;
     text-align: center;
     h1 {
-      margin-bottom: 24px;S
-    }
-    input {
-      background: #232129;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      padding: 16px;
-      width: 100%;
-      color: #f4ede8;
-      &::placeholder {
-        color: #666360;
-
-      }
-      & + input {
-        margin-top: 8px;
-      }
-    }
-    button {
-      background: #ff9000;
-      height: 56px;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      color: #312e38;
-      width: 100%;
-      font-weight: 500;
-      margin-top: 16px;
-      transition: background-color 0.2s;
-      &:hover {
-        background: ${shade(0.2, '#ff9000')};
-      }
+      margin-bottom: 24px;
     }
     > a {
       color: #f4ede8;
-      display:block;
-      margin-top:24px;
+      display: block;
+      margin-top: 24px;
       text-decoration: none;
       transition: color 0.2s;
       &:hover {
-        color: ${shade(0.2, '#f4ede8')}
+        color: ${shade(0.2, '#f4ede8')};
       }
     }
   }
   > a {
     color: #ff9000;
-      display:block;
-      margin-top:24px;
-      text-decoration: none;
-      transition: color 0.2s;
-      display:flex;
-      align-items: center;
-      svg {
-        margin-right: 16px;
-      }
-      &:hover {
-        color: ${shade(0.2, '#ff9000')};
-      }
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+    display: flex;
+    align-items: center;
+    svg {
+      margin-right: 16px;
+    }
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
   }
 `;
 export const Background = styled.div`
