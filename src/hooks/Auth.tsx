f55aea8a404b6423f88d9d96e07a28ce518333d0
Renato Bessa <bessache@outlook.com>
2020-06-13 12:22:19 -0300

    commita aula Disponibilidade de mes

--- src/hooks/Auth.tsx
+++ src/hooks/Auth.tsx
@@ -29,8 +29,10 @@ const AuthProvider: React.FC = ({ children }) => {
     const token = localStorage.getItem('@GoBarber:token');
     const user = localStorage.getItem('@GoBarber:user');
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
       return { token, user: JSON.parse(user) };
     }
+
     return {} as AuthState;
   });
   const signIn = useCallback(async ({ email, password }) => {
@@ -41,6 +43,8 @@ const AuthProvider: React.FC = ({ children }) => {
     const { token, user } = response.data;
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
