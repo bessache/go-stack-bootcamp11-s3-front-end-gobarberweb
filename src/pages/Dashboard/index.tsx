6a638e51813c383bc9aba000385e5ba038fc16a6
Renato Bessa <bessache@outlook.com>
2020-06-21 10:26:41 -0300

    commit aula Pagina de perfil de usuario

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -7,6 +7,7 @@ import 'react-day-picker/lib/style.css';
 
 import { FiPower, FiClock } from 'react-icons/fi';
 
+import { Link } from 'react-router-dom';
 import {
   Container,
   Header,
@@ -135,7 +136,9 @@ const Dashboard: React.FC = () => {
             <img src={user.avatar_url} alt={user.name} />
             <div>
               <span>Bem-Vindo</span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
           <button type="button" onClick={signOut}>
